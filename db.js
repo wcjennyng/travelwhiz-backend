8c9fe6c7b1b58f6717c685677e0f29465851b251
Jenny Ng <wcjennyng@gmail.com>
2023-02-25 12:42:36 -0500

    updating db

--- db.js
+++ db.js
@@ -5,10 +5,20 @@
 const { Client } = require("pg")
 const { getDatabaseUri } = require("./config")
 
+let db;
 
-const db = new Client({
-    connectionString: getDatabaseUri()
-});
+if (process.env.NODE_ENV === "production") {
+    db = new Client({
+        connectionString: getDatabaseUri(),
+        ssl: {
+            rejectUnauthorized: false,
+        },
+    });
+} else {
+    db = new Client({
+        connectionString: getDatabaseUri()
+    })
+}
 
 db.connect();
 
